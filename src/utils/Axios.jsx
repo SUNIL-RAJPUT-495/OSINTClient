3a2a593034dca82831108b4c224e7a5b66492e2d
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-01-30 21:50:40 +0530

    according to subham sir done

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -3,17 +3,13 @@ import { baseURL } from "../common/SummeryApi";
 
 const Axios = axios.create({
     baseURL: baseURL,
-    withCredentials: true // Cookies support (PC/Laptop ke liye)
+    withCredentials: true 
 });
 
-// --- INTERCEPTOR (MOBILE MAGIC FIX) ---
-// Har request bhejne se pehle ye function chalega
 Axios.interceptors.request.use(
     (config) => {
-        // 1. LocalStorage se token nikalo
         const token = localStorage.getItem("access_token");
 
-        // 2. Agar token mila, toh Header mein daal do
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
